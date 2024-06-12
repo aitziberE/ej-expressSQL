d375cf9556c7febd99086050ab18fc10b5e23e8b
aitziberE <eskizabel.aitziber@gmail.com>
2024-06-12 20:43:01 +0200

    ej4

---
+++ ej1/ej4.js
@@ -0,0 +1,79 @@
+const express = require("express")
+const app = express()
+const mysql = require('mysql2')
+app.use(express.json())
+
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'abcd*1234',
+    database:  'ej1DB'
+})
+
+db.connect()
+
+// Crea un endpoint que muestre todos los productos
+app.get('/products', (req, res) => {
+    const sql = `SELECT * FROM products`
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        res.send({ message: 'Get products', result })
+      })
+})
+
+// Crea un endpoint que muestre todas las categorías
+app.get('/categories', (req, res) => {
+    const sql = `SELECT * FROM categories`
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        res.send({ message: 'Get categories', result })
+      })
+})
+
+// Crea un endpoint que muestra todos los productos con sus categorías
+app.get('/all', (req, res) => {
+    const sql = `SELECT products.id, products.name AS product, products.price, categories.name AS category
+    FROM products
+    INNER JOIN categories ON products.category_id = categories.id`
+
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        res.send({ message: 'Get all', result })
+      })
+})
+
+// Crea un endpoint donde puedas seleccionar un producto por id
+app.get('/products/id/:id', (req, res) => {
+    const sql = `SELECT * FROM products WHERE id = ${req.params.id}`
+    db.query(sql, (err, result) => {
+      if (err) throw err
+      res.send(result)
+    })
+})
+
+// Crea un endpoint que muestre de forma descendente los productos.
+app.get('/products', (req, res) => {
+    const sql = `SELECT * FROM products ORDER BY name DESC`
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        res.send({ message: 'Get products', result })
+      })
+})
+
+// Crea un endpoint donde puedas seleccionar una categoría por id
+app.get('/categories/id/:id', (req, res) => {
+    const sql = `SELECT * FROM categories WHERE id = ${req.params.id}`
+    db.query(sql, (err, result) => {
+      if (err) throw err
+      res.send(result)
+    })
+})
+
+// Crea un endpoint donde puedas buscar un producto por su nombre
+app.get('/products/name/:name', (req, res) => {
+    const sql = `SELECT * FROM products WHERE name = ${req.params.name}`
+    db.query(sql, (err, result) => {
+      if (err) throw err
+      res.send(result)
+    })
+})
